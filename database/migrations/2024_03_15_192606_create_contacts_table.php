b8c082e683476d5ec40b217a54e2007d61311688
Mahdi Amereh <mahdieightt@gmail.com>
2024-03-15 23:20:26 +0330

    update contact migration

--- database/migrations/2024_03_15_192606_create_contacts_table.php
+++ database/migrations/2024_03_15_192606_create_contacts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\General\GenderEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,11 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->enum('gender', GenderEnum::values());
             $table->timestamps();
         });
     }
